e01cec209c7285dd663dbae33e916a24ea8b7ba5
Pierre Marcenac <pierre.marcenac@centraliens.net>
2019-05-19 19:26:55 +0200

    Add binary to start from shell

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ const EventEmitter = require('events');
 
 const { initWidgets } = require('./widgets');
 
-const eventMiddleware = new EventEmitter();
-initWidgets(eventMiddleware);
+const kubeasy = () => {
+  const eventMiddleware = new EventEmitter();
+  initWidgets(eventMiddleware);
+};
+
+module.exports = {
+  kubeasy,
+};
