e9a20cf42699e3f4576f474a82363ddea67c0a7a
Pierre Marcenac <pierre.marcenac@centraliens.net>
2019-06-04 19:27:12 +0200

    Setup unit tests

---
+++ src/hooks/tests/pods.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('[HOOKS] Pods', () => {
+  describe('formatData', () => {
+    it('should extract status', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
